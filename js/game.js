2c8dbf9622f34d63522a3226e0d7111cc81b751d
Hassen <hbenguenna@edenschool.fr>
2019-10-01 17:38:38 +0200

    bientot finis

--- js/game.js
+++ js/game.js
@@ -1,80 +1,29 @@
-var zero = document.getElementById("0");
-var un = document.getElementById("1");
-var deux = document.getElementById("2");
-var trois = document.getElementById("3");
-var quatre = document.getElementById("4");
-var cinq = document.getElementById("5");
-var six = document.getElementById("6");
-var sept = document.getElementById("7");
-var huit = document.getElementById("8");
-var neuf = document.getElementById("9");
-var dix = document.getElementById("10");
-var onze = document.getElementById("11");
+var tabs = ["red","blue","yellow","green","orange","white"]; // taille: 6
 
-var tabs = ["red","blue","yellow","green","pink","white"];
-
-
-function rouge() {
-
-    for (let i = 0; i < tabs.length; i++) {
-        zero.style.background =  tabs[Math.floor(Math.random() * (tabs.length - 0 + 1) + 0)];
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
     }
-
-    if (tabs != tabs) {
-        zero.style.background = "blue";
-    }
-
+    return a;
 }
 
+var shuffleArray = shuffle(tabs.concat(tabs));
+console.log(shuffleArray);
 
 
-function bleu() {
-    un.style.background = "red";
 
+// selectionner tte les carte
+var card = document.getElementsByClassName("card");
+console.log(card);
 
+for (i in shuffleArray) {
+        card[i].classList.add(shuffleArray[i]);
+        card[i].addEventListener('click', function(event) {
+            event.target.classList.add("bg-" + event.target.classList[1])
+        })
 }
-
-function vert() {
-    deux.style.background = "green";
-
-    
-}
-
-function jaune() {
-    trois.style.background = "yellow";
-
-}
-
-function orange() {
-    quatre.style.background = "orange";
-
-
-}
-
-function rose() {
-    cinq.style.background = "pink";
-
-
-}
-
-function blanc() {
-    six.style.background = "white";
-
-   
-}
-
-
-
-
-
-
-
-
-zero.addEventListener("click",rouge);
-un.addEventListener("click",bleu);
-deux.addEventListener("click",vert);
-trois.addEventListener("click",jaune);
-quatre.addEventListener("click",orange);
-cinq.addEventListener("click",rose);
-six.addEventListener("click",blanc);
 
